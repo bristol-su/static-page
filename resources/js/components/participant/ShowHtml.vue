6f3ce7726bdd522ca5e6d97d0b6af9450e6dc52e
Aidan Laycock <aidanl_@hotmail.co.uk>
2021-09-01 10:11:31 +0100

    Participant Styling and accessibility

--- resources/js/components/participant/ShowHtml.vue
+++ resources/js/components/participant/ShowHtml.vue
@@ -1,37 +1,36 @@
 <template>
-    <div>
-        <span v-html="html"></span>
-
-        <div v-if="canClickButton" style="text-align: right;">
+    <div class="w-full p-3">
+        <span class="p-5" v-html="html"></span>
+        <div v-if="canClickButton">
             <submit-button :can-unsubmit="canUnsubmit">{{buttonText}}</submit-button>
         </div>
     </div>
 </template>
 
 <script>
-    import SubmitButton from './SubmitButton';
-    export default {
-        name: "ShowHtml",
-        components: {SubmitButton},
-        props: {
-            html: {
-                required: true,
-                type: String
-            },
-            buttonText: {
-                required: true,
-                type: String,
-            },
-            canClickButton: {
-                required: true,
-                type: Boolean
-            },
-            canUnsubmit: {
-                required: true,
-                type: Boolean
-            }
+import SubmitButton from './SubmitButton';
+export default {
+    name: "ShowHtml",
+    components: {SubmitButton},
+    props: {
+        html: {
+            required: true,
+            type: String
         },
-    }
+        buttonText: {
+            required: true,
+            type: String,
+        },
+        canClickButton: {
+            required: true,
+            type: Boolean
+        },
+        canUnsubmit: {
+            required: true,
+            type: Boolean
+        }
+    },
+}
 </script>
 
 <style scoped>
--- resources/views/participant.blade.php
+++ resources/views/participant.blade.php
@@ -4,11 +4,13 @@
 
 @section('module-content')
     <p-page-content title="{{settings('title')}}" subtitle="{{settings('subtitle')}}">
-        <show-html
-            html="{{settings('html', 'No content has been set.')}}"
-            :can-click-button="{{(app(\BristolSU\Support\Permissions\Contracts\PermissionTester::class)->evaluate('static-page.click-button')?'true':'false')}}"
-            :can-unsubmit="{{(app(\BristolSU\Support\Permissions\Contracts\PermissionTester::class)->evaluate('static-page.delete-button-click')?'true':'false')}}"
-            button-text="{{settings('button_text', 'Submit')}}"></show-html>
+        <p-card-group title="{{ settings('title') }}">
+            <show-html
+                html="{{settings('html', 'No content has been set.')}}"
+                :can-click-button="{{(app(\BristolSU\Support\Permissions\Contracts\PermissionTester::class)->evaluate('static-page.click-button')?'true':'false')}}"
+                :can-unsubmit="{{(app(\BristolSU\Support\Permissions\Contracts\PermissionTester::class)->evaluate('static-page.delete-button-click')?'true':'false')}}"
+                button-text="{{settings('button_text', 'Submit')}}"></show-html>
+        </p-card-group>
     </p-page-content>
 @endsection
 
